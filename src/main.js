0079ec88ba1df2495aad14d371f59eb09129abbb
Luisf5588 <Luisfqm@permoda.com.co>
2020-12-15 10:06:29 -0500

    Validacion loguin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+const base = axios.create({
+  baseURL : 'HTTPS://glacial-everglades-74306.herokuapp.com'
+})
 
 Vue.config.productionTip = false
+Vue.prototype.$http = base;
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+        <the-login></the-login>
+
+    </div>
+</template>
+
+<script>
+import TheLogin from '@/components/auth/TheLogin'
+// import Thelogin from '../components/auth/Thelogin.vue'
+export default {
+    components:{
+        TheLogin
+
+    }
+}
+</script>
