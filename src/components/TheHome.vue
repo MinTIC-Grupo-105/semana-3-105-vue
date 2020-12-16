7e597c15d7f75985b652959beb335af888851c28
Santiago Rendon Valencia <santiago.rendon@fau.de>
2020-12-16 09:32:18 -0500

    actualizacion de home

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,36 +1,63 @@
 <template>
-<div>    
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img
+            src="bootstrap-solid.svg"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt=""
+          />
+          Marketing Digital
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    
-    <form class="form-inline my-2 my-lg-0">
-     
-      <button 
-      class="btn btn-outline-success my-2 my-sm-0" 
-      type="submit"
-      @click.prevent="logOut"
-      >Salir</button>
-    </form>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <!-- <a class="nav-link" data-toggle="modal" data-target="#loginModal" href="#">Ingreso</a> -->
+              <button
+                class="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+                @click.prevent="logOut"
+              >
+                Salir
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="span3 well">
+        <center>
+          <a href="#aboutModal" data-toggle="modal" data-target="#myModal"
+            ><img
+              src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R"
+              name="aboutme"
+              width="140"
+              height="140"
+              class="rounded-circle"
+          /></a>
+          <h3>{{ user.nombre }}</h3>
+          <em>{{ user.email }}</em>
+        </center>
+      </div>
+    </div>
   </div>
-</nav>
-
-<div class="container">
-    <p>
-        {{user.nombre}}
-    </p>
-    <p>
-        {{user.email}}
-    </p>
-
-
-</div>
-
-</div>
 </template>
 
 <script>
@@ -39,33 +66,29 @@ $(document).ready(function () {
 });
 
 export default {
-    data(){
-        return{
-            user:{}
-        }
+  data() {
+    return {
+      user: {},
+    };
+  },
 
+  methods: {
+    getUserDetails() {
+      let user = localStorage.getItem("user");
+      let token = localStorage.getItem("jwt");
+
+      if (token) {
+        this.user = JSON.parse(user);
+      }
     },
-
-    methods:{
-        getUserDetails(){
-            let user = localStorage.getItem('user');
-            let token = localStorage.getItem('jwt');
-
-            if(token){
-                this.user = JSON.parse(user);
-            }
-        },
-        logOut(){
-            localStorage.removeItem('jwt');
-            localStorage.removeItem('user');
-            this.$router.push('/');
-
-    }
+    logOut() {
+      localStorage.removeItem("jwt");
+      localStorage.removeItem("user");
+      this.$router.push("/");
     },
-    created(){
-        this.getUserDetails();
-    }
-}
-
-
+  },
+  created() {
+    this.getUserDetails();
+  },
+};
 </script>
--- src/components/auth/TheLogin.vue
+++ src/components/auth/TheLogin.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="container" mt="5">
+  <div class="" mt="5">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Marketing Digital
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <!-- <a class="nav-link" data-toggle="modal" data-target="#loginModal" href="#">Ingreso</a> -->
+            <button
+            type="button"
+            class="btn btn-info btn-round"
+            data-toggle="modal"
+            data-target="#loginModal"
+          >
+            Login
+          </button>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Registro</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
+
+
+
+
+
     <div
       class="modal fade"
       id="loginModal"
